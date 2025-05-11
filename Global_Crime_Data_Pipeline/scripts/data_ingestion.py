542ad3912cb6d16331865c682430b3649d4e0736
Shraddha0925 <shraddha_p09@hotmail.com>
2025-05-11 12:58:54 +0100

    Update data_ingestion.py

--- Global_Crime_Data_Pipeline/scripts/data_ingestion.py
+++ Global_Crime_Data_Pipeline/scripts/data_ingestion.py
@@ -2,31 +2,20 @@ import requests
 import pandas as pd
 import yaml
 import os
+from datetime import datetime
+
+# Create a log file to store the output
+log_file = '../logs/data_ingestion_log.txt'
+os.makedirs('../logs/', exist_ok=True)
+
+def log_message(message):
+    with open(log_file, 'a') as log:
+        log.write(f"{datetime.now()} - {message}\n")
+    print(message)
 
 # Load configurations
-with open('../configs/config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-
-# API Endpoint
-url = config['api_endpoint']
-
-# Fetch Data
-print("Fetching data from API...")
-response = requests.get(url)
-
-if response.status_code == 200:
-    print("Data fetched successfully!")
-    data = response.json()
-    
-    # Convert to DataFrame
-    df = pd.DataFrame(data)
-    
-    # Create directory if not exists
-    os.makedirs('../data/raw/', exist_ok=True)
-    
-    # Save to CSV
-    df.to_csv('../data/raw/crime_data.csv', index=False)
-    print("Data saved to data/raw/crime_data.csv")
-else:
-    print("Failed to fetch data. Status Code:", response.status_code)
-
+try:
+    with open('../configs/config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    log_message("Configurations loaded successfully.")
+ex
