5075e6b767926bb5ec15ecfc7d7d2d158b48713c
Shraddha0925 <shraddha_p09@hotmail.com>
2025-05-11 12:43:44 +0100

    Update data_ingestion.py

--- Global_Crime_Data_Pipeline/scripts/data_ingestion.py
+++ Global_Crime_Data_Pipeline/scripts/data_ingestion.py
@@ -1,1 +1,32 @@
+import requests
+import pandas as pd
+import yaml
+import os
+
+# Load configurations
+with open('../configs/config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# API Endpoint
+url = config['api_endpoint']
+
+# Fetch Data
+print("Fetching data from API...")
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Data fetched successfully!")
+    data = response.json()
+    
+    # Convert to DataFrame
+    df = pd.DataFrame(data)
+    
+    # Create directory if not exists
+    os.makedirs('../data/raw/', exist_ok=True)
+    
+    # Save to CSV
+    df.to_csv('../data/raw/crime_data.csv', index=False)
+    print("Data saved to data/raw/crime_data.csv")
+else:
+    print("Failed to fetch data. Status Code:", response.status_code)
 
